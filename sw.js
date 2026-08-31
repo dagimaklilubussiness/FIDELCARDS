8eda24b461cfa63a2a669fd23623fc6db06f3055
dagimaklilubussiness <dagimaklilubussiness@gmail.com>
2026-09-01 02:10:10 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'fidelcards-v8';
+const CACHE_NAME = 'fidelcards-v13';
 const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', (event) => {
